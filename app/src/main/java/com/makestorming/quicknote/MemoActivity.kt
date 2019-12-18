ce83231eaee234e6b64ac58d8004f7a69463a967
overman623 <overman623@naver.com>
2019-12-18 16:36:06 +0900

    feat : Set Structure MemoActivity

--- app/src/main/java/com/makestorming/quicknote/MemoActivity.kt
+++ app/src/main/java/com/makestorming/quicknote/MemoActivity.kt
@@ -1,28 +1,33 @@
 package com.makestorming.quicknote
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.content_memo.*
+
 
 class MemoActivity : AppCompatActivity() {
 
+    private var date : String? = null
+    private var title : String? = null
+    private var order : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memo)
 
-        intent.getStringExtra("DATE")
-        intent.getStringExtra("TITLE")
-        intent.getIntExtra("ORDER", 0)
+        date = intent.getStringExtra("DATE")
+        title = intent.getStringExtra("TITLE")
+        order = intent.getIntExtra("ORDER", 0)
 
     }
 
-
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_memo, menu)
         return true
     }
 
@@ -31,9 +36,29 @@ class MemoActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                //setting dialog
+                true
+            }
+            R.id.action_font_size -> {
+                //font size dialog
+                true
+            }
+            R.id.action_erase_all -> {
+                //erase field
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
+
     }
+
+
+    override fun onBackPressed() {
+        var before = editText.text.toString()
+        if(before == title) //show dialog : would you save the text
+        super.onBackPressed()
+    }
+
 
 }
